3acec58977cfa1afb82accad231123e84387e4a9
Nick Albinati <nickalbinati@Nicks-MacBook-Pro.local>
2022-04-17 22:05:47 -0700

    first commit

---
+++ script/Lab10_part1.js
@@ -0,0 +1,17 @@
+function changeColor(colorFont){
+
+    var colorOne = document.getElementById("text")
+    text.style.color=colorFont;
+
+}
+
+
+function changeFontColor(){
+}
+
+
+
+function backgroundChangeColor(colorBackground){
+    var colorTwo = document.getElementById("td_text")
+    colorTwo.style.backgroundColor=colorBackground;
+}
